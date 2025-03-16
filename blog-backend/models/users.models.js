9b7de70fa7d61c8ec37c285828ef6f7acfd42872
Md Noorullah Raza <mnraza94@gmail.com>
2025-03-17 00:22:54 +0530

    initialize blog-backend with basic structure and user model

---
+++ blog-backend/models/users.models.js
@@ -0,0 +1,27 @@
+const { default: mongoose } = require("mongoose");
+
+const userSchema = new mongoose.Schema(
+  {
+    username: {
+      type: String,
+      unique: true,
+      required: true,
+      trim: true,
+    },
+    email: {
+      type: String,
+      unique: true,
+      required: true,
+      trim: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+export const User = mongoose.model("User", userSchema);
