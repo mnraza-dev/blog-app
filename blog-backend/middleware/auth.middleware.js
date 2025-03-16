bf1502389b1c54bd2f9c6a5cfe75cb65f00c1c8c
Md Noorullah Raza <mnraza94@gmail.com>
2025-03-17 02:13:17 +0530

    update authentication middleware to extract token using split; secure login route with auth middleware

--- blog-backend/middleware/auth.middleware.js
+++ blog-backend/middleware/auth.middleware.js
@@ -2,7 +2,7 @@ import jwt from "jsonwebtoken";
 
 const auth = async (req, res, next) => {
   try {
-    const token = req.header("Authorization").replace("Bearer ", "");
+    const token = req.header("Authorization").split(" ")[1];
     const decoded = jwt.verify(token, process.env.JWT_SECRET);
     req.userId = decoded.userId;
     next();
--- blog-backend/routes/auth.routes.js
+++ blog-backend/routes/auth.routes.js
@@ -1,8 +1,9 @@
 import { Router } from "express";
 import { login, register } from "../controllers/auth.controllers.js";
+import auth from "../middleware/auth.middleware.js";
 const authRoutes = Router();
 
 authRoutes.post("/register", register);
-authRoutes.post("/login", login);
+authRoutes.post("/login",auth, login);
 
 export default authRoutes;
